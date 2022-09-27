413a80148e87cfc91859f779316b642e3bf88ba5
Osman Kaan P <87131543+cr0mm@users.noreply.github.com>
2022-09-27 10:19:26 +0100

    Add files via upload

---
+++ SpeezleGame.Launcher/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new SpeezleGame.Launcher.SpeezleGame();
+game.Run();
---
+++ SpeezleGame.Launcher/SpeezleGame.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpeezleGame.Graphics;
+
+namespace SpeezleGame.Launcher
+{
+    public class SpeezleGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _characterSpriteSheet;
+        private SpriteAnimation _characterAnim;
+        public SpeezleGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            _characterSpriteSheet = Content.Load<Texture2D>("Textures/AnimationTestBigCharacter"); 
+            _characterAnim = new SpriteAnimation(_characterSpriteSheet, 4, 32, 32)
+            {
+                Fps = 4
+            };
+
+            _characterAnim.Play();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+            _characterAnim.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+
+            _spriteBatch.Begin();
+            _characterAnim.Draw(_spriteBatch, new Vector2(100, 100));
+            _spriteBatch.End();
+        }
+    }
+}
